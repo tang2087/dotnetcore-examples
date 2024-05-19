bbe2b36cb3d54b90555e5e02fc029ad89154858c
Raymond Tang <fahao.tang@hotmail.com>
2024-05-19 12:02:02 +1000

    Moved Migrations folder to the ef_migrations project and changed migration assembly accordingly.

--- ef-migrations-web/Program.cs
+++ ef-migrations-web/Program.cs
@@ -1,10 +1,11 @@
 using System.Reflection;
 using ef_migrations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlite("Data Source=app.db", b=>{
-    b.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+    b.MigrationsAssembly(Assembly.GetAssembly(typeof(ef_migrations.Migrations.InitMigrations)).FullName);
 }));
 var app = builder.Build();
 
